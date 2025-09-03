5030c632062a1563f6bdc6979006d9641060d654
Brandon Sueur <gynidark@gmail.com>
2025-09-04 00:03:20 +0200

    fix: toto

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,3 @@
 export * from "./banner";
-export * from "./deleteConfirmationModal";
-export * from "./establishmentCard";
 export * from "./faq";
-export * from "./legalLayout";
 export * from "./modal";
