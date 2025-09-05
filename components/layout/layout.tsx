a7c369760b1a04350b9318acea954befc9c072f9
Brandon Sueur <gynidark@gmail.com>
2025-09-05 23:32:59 +0200

    feat: push

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -14,13 +14,16 @@ export const Layout = ({ children, className }: LayoutProps) => {
     <div className="relative min-h-screen flex flex-col bg-white">
       <div className="bg-primary fixed inset-x-0 top-0 z-50 isolate">
         <Container className="text-white p-2 flex justify-between text-sm md:text-md">
-          <p className="w-60 md:w-96 text-sm">
+          <p className="w-60 md:w-96 text-xs md:text-sm">
             Urgence <span className="font-bold">électricité</span> ou{" "}
             <span className=" font-bold">plomberie</span> ?{" "}
             <span>Intervention rapide 7j/7</span>
           </p>
 
-          <a href="tel:0365889783" className="flex items-center gap-4">
+          <a
+            href="tel:0365889783"
+            className="flex items-center gap-4 text-xs md:text-sm"
+          >
             <Phone width={18} height={18} />
             03 65 88 97 83{" "}
             <span className="hidden md:flex">/ 06 40 87 15 53</span>
